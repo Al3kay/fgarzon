d1dba8f20349cd3098bee43fbf6faae979f14c94
Al3kay <alexdigarzon@hotmail.com>
2020-04-19 19:31:30 -0500

    changing page

--- js/news_gen.js
+++
@@ -1,6 +0,0 @@
-document.write('<ul id="ticker">');
-var x=xmlDoc.getElementsByTagName("item");
-for (i=0;i<x.length;i++){
-document.write('<li>'+ x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +'</li>');
-}
-document.write('</ul>');;
--- js/ticker_start.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-        $('#ticker').list_ticker({
-                speed:10000,
-                effect:'fade'
-        })              
-})
